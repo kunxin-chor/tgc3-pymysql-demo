2e36c20d142c8a84b5de27323c7a7875ebc97836
Ubuntu <ubuntu@ip-172-31-4-208.ap-southeast-1.compute.internal>
2019-10-15 07:25:17 +0000

    Added functionality to insert a new artist

--- python-mysql-demo/add-artist.py
+++ python-mysql-demo/add-artist.py
@@ -6,3 +6,15 @@ connection = pymysql.connect(host='localhost', #localhost means the same machine
     password="password",
     database="chinook"
 )
+
+artist_name = input("Please enter the artist name > ")
+
+cursor = connection.cursor(pymysql.cursors.Cursor)
+cursor.execute("SELECT MAX(ArtistId) FROM Artist")
+max_id = cursor.fetchone()[0]
+next_id = max_id + 1
+
+sql = "INSERT INTO Artist (ArtistId, Name) VALUES ({},'{}')".format(next_id, artist_name)
+cursor.execute(sql)
+
+connection.commit()
