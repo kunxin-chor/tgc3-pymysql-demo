7dd84edaf7188c17eb3075f3616525197084838c
Ubuntu <ubuntu@ip-172-31-4-208.ap-southeast-1.compute.internal>
2019-10-15 06:33:41 +0000

    Added dict cursor

--- python-mysql-demo/app.py
+++ python-mysql-demo/app.py
@@ -1,8 +1,14 @@
 import pymysql
 
 # represents the connection between the database and Python
-connection = pymysql.connect(host='localhost',
-    user="admin",
+connection = pymysql.connect(host='localhost', #localhost means the same machine as we are on now
+    user="admin", 
     password="password",
-    database="Chinook"
+    database="chinook"
 )
+
+cursor = connection.cursor(pymysql.cursors.DictCursor)
+cursor.execute("SELECT * from Employee")
+for row in cursor:
+    # print ("EmplyoeeID:",row[0]," is ",row[1],row[2])
+    print("EmployeeID:{} is {} {}".format(row["EmployeeId"], row["LastName"], row["FirstName"]))
